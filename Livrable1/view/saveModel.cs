1c4e8f7126b044315fb97a2e41a70b33074f7313
elavaux0 <elavaux0@gmail.com>
2023-02-03 15:28:31 +0100

    first push app

---
+++ Livrable1/view/saveModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Livrable1.view
+{
+    internal class SaveModel
+    {
+        string name;
+        string source;
+        string target;
+        int method;
+        public void saveModel(string name, string source, string target, int methode) { 
+            this.name = name;
+            this.source = source;
+            this.target = target;
+            this.method = methode;
+        }
+    }
+}
