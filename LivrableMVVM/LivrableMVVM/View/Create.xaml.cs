fe645957482e6716c90964d4c42b567efae719b5
Hugo Vallemont <hugo.vallemont@viacesi.fr>
2023-02-09 11:23:22 +0100

    add winform + button to open explorer and write menu selected in textbox

--- LivrableMVVM/LivrableMVVM/View/Create.xaml.cs
+++ LivrableMVVM/LivrableMVVM/View/Create.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,6 +9,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -18,11 +21,38 @@ namespace LivrableMVVM.View
     /// <summary>
     /// Logique d'interaction pour Create.xaml
     /// </summary>
-    public partial class Create : UserControl
+    public partial class Create : System.Windows.Controls.UserControl
     {
         public Create()
         {
             InitializeComponent();
         }
+
+        public void OnSourceClicked(object sender, RoutedEventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    this.Source.Text = fbd.SelectedPath;
+                }
+            }
+        }
+
+        public void OnTargetClicked(object sender, RoutedEventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    this.Target.Text = fbd.SelectedPath;
+                }
+            }
+        }
+
     }
 }
