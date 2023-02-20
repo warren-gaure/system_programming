9e23ef18221d19d0bfaefa178e68918f652a5ef4
Hugo Vallemont <hugo.vallemont@viacesi.fr>
2023-02-20 14:49:47 +0100

    change colors

--- LivrableMVVM/LivrableMVVM/MainWindow.xaml.cs
+++ LivrableMVVM/LivrableMVVM/MainWindow.xaml.cs
@@ -7,11 +7,13 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TrayNotify;
 
 namespace LivrableMVVM
 {
@@ -24,7 +26,6 @@ namespace LivrableMVVM
         {
             InitializeComponent();
             DataContext = this;
-         
         }
 
    
