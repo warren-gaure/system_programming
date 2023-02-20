de2d734c81eba9ea5fa522e36acf2a20f48e5338
Mallory Gesbert <mallorygesbert@gmail.com>
2023-02-20 14:02:04 +0100

    Add all models from MVC

---
+++ LivrableMVVM/LivrableMVVM/Model/IObserver.cs
@@ -0,0 +1,15 @@
+﻿using LivrableMVVM.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.JavaScript;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace livrableMVC.Model
+{
+    internal interface IObserver
+    {
+        void Update(ISubject subject);        // Receive update from subject
+    }
+}
