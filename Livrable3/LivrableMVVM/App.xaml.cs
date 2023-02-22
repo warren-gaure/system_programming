a0f11c06801a1bb81e37fbcd021ad2878082f560
Warren GAURE <w.gaure@gmail.com>
2023-02-22 08:56:38 +0100

    Forcing the application to a single instance

--- Livrable3/LivrableMVVM/App.xaml.cs
+++ Livrable3/LivrableMVVM/App.xaml.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -14,15 +15,22 @@ namespace Livrable3
     /// </summary>
     public partial class App : Application
     {
-
+        private static string appID = "eb3bc824-ef72-482d-bc27-0580d5825c23";
+        Mutex mutex = new Mutex(false, appID);
         protected override void OnStartup(StartupEventArgs e)
         {
+            if (!mutex.WaitOne(0, false))
+            {
+                MessageBox.Show("An instance is already running.");
+                return;
+            }
 
             MainWindow = new MainWindow()
             {
                 DataContext = new MainViewModel()
             };
 
+            GC.Collect();
             MainWindow.Show();
             base.OnStartup(e);
         }
